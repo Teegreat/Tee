754780ddc786401f6e0bea08643d74e6daa8a00a
Teegreat <toyinatofarati64@gmail.com>
2020-01-02 11:31:47 +0100

    typist effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,24 @@
 import React from "react"
 import Layout from "../components/layout"
 import Head from "../components/head"
+import Typist from "react-typist"
 
 const HomePage = () => {
   return (
     <Layout>
       <Head title="Home" />
-      <h1>Hi there,</h1>
-      <h2>I'm Tee a web developer living in Nigeria</h2>
+      <h1>
+        <Typist avgTypingDelay={130} stdTypingDelay={30}>
+          <span>Web Consultant...</span>
+          <Typist.Backspace count={17} delay={3000} />
+          <span>Web Developer...</span>
+          <Typist.Backspace count={22} delay={3000} />
+          <span>WordPress Developer</span>
+        </Typist>
+      </h1>
+
+      <h2 style={{ marginTop: "2.5rem" }}>Hello there!</h2>
+      <h2>I'm Tee and I derive joy in building websites that rocks!!!</h2>
     </Layout>
   )
 }
