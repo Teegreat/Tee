5363bfb8351464dda6b76f265f95ab6971d3e49a
Teegreat <toyinatofarati64@gmail.com>
2020-01-03 15:00:11 +0100

    about page

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
       },
     },
     "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-transformer-remark",
       options: {
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,72 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
+import aboutStyle from "./about.module.scss"
 import Layout from "../components/layout"
+import indexStyles from "./index.module.scss"
+
 import Head from "../components/head"
 
-const aboutPage = () => {
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "images/mandev.jpg" }) {
+      childImageSharp {
+        fixed(width: 150) {
+          src
+        }
+      }
+    }
+  }
+`
+
+const aboutPage = props => {
+  const profilePicture = (
+    <img
+      src={props.data.file.childImageSharp.fixed.src}
+      alt=""
+      className={aboutStyle.profilePicture}
+    />
+  )
   return (
     <Layout>
       <Head title="About Me" />
       <h1>About Me</h1>
-      <p>Self taught developer with great passion for web development.</p>
+      <div>{profilePicture}</div>
+      <p>
+        I'm a Self taught front-end web developer based in Abuja, Nigeria. I do
+        work remotely for both local and international clients.
+      </p>
       <p>
         <Link to="/contact">Want to work with me? Reach out</Link>
       </p>
+
+      <br />
+      <br />
+      <br />
+      <hr
+        style={{
+          backgroundImage:
+            "linear-gradient(to right, #6a11cb 0%, #2575fc 100%)",
+          height: ".1rem",
+        }}
+      />
+      <br />
+      <br />
+      <h1>Why Work With Me</h1>
+      <p>
+        If there's one thing I love doing, is developing good websites for my
+        clients to give them a unique online presence.
+      </p>
+      <p>
+        I've spent many years trying to be perfect at what I do, and while I'll
+        never be perfect, I do my very best to give you the best.
+      </p>
+      <p>Want to work with me?</p>
+      <button className={indexStyles.button}>
+        <Link className={indexStyles.button} to="/contact">
+          Let's Talk !
+        </Link>
+      </button>
     </Layout>
   )
 }
